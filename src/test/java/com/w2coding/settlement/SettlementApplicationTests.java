fc0324ba595b0696a1c87769eb5b139dd524002a
injuhan <haninjuin@gmail.com>
2023-01-29 19:38:00 +0900

    first commit

---
+++ src/test/java/com/w2coding/settlement/SettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2coding.settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
