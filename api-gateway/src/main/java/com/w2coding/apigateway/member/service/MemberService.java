74b19daeb15ec875a2c3065539b30e2a7d301556
HoYoon-Lee <happylhy0630@naver.com>
2023-03-26 20:10:26 +0900

    FEATURE : add signUp() method logic

--- api-gateway/src/main/java/com/w2coding/apigateway/member/service/MemberService.java
+++ api-gateway/src/main/java/com/w2coding/apigateway/member/service/MemberService.java
@@ -1,17 +1,25 @@
 package com.w2coding.apigateway.member.service;
 
-import org.springframework.stereotype.Service;
-
+import com.w2coding.apigateway.common.exception.GrpcException;
 import com.w2coding.apigateway.member.dto.SignUpDto;
-
+import com.w2coding.proto.common.GrpcResponse;
+import com.w2coding.proto.member.MemberServiceGrpc;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
 
-	public void signUp(SignUpDto signUpDto) {
+	private final MemberServiceGrpc.MemberServiceBlockingStub memberServiceBlockingStub;
 
+	public void signUp(SignUpDto signUpDto) {
+		GrpcResponse response = memberServiceBlockingStub.signUp(signUpDto.toSignUpRequest());
+		switch (response.getResult()) {
+			case "S_ID001" ->
+					throw new GrpcException(response.getResult(), response.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
