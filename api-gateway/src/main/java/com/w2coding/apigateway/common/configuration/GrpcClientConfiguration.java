2f3aa679ca46ab70985cd89480f16d3d018dd732
injuhan <haninjuin@gmail.com>
2023-04-01 22:08:42 +0900

    CHORE : move settlementServer grpc host and port to application.yml

--- api-gateway/src/main/java/com/w2coding/apigateway/common/configuration/GrpcClientConfiguration.java
+++ api-gateway/src/main/java/com/w2coding/apigateway/common/configuration/GrpcClientConfiguration.java
@@ -3,13 +3,20 @@ package com.w2coding.apigateway.common.configuration;
 import com.w2coding.proto.member.MemberServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GrpcClientConfiguration {
+    @Value("${grpc.settlementServer.host}")
+    private String settlementServer;
 
-    private final ManagedChannel settlementServerChannel = ManagedChannelBuilder.forAddress("localhost", 8082)
+    @Value("${grpc.settlementServer.port}")
+    private int settlementPort;
+
+    private final ManagedChannel settlementServerChannel = ManagedChannelBuilder.forAddress(settlementServer, settlementPort)
                 .usePlaintext()
                 .build();
 
