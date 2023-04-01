32bdceebb88b5ec0618745d5f79816b83fdeb5dd
injuhan <haninjuin@gmail.com>
2023-04-01 22:16:14 +0900

    FEATURE : add WorkerService

---
+++ settlement-server/src/main/java/com/w2coding/settlementserver/member/service/WorkerService.java
@@ -0,0 +1,14 @@
+package com.w2coding.settlementserver.member.service;
+
+import org.springframework.stereotype.Service;
+
+import com.w2coding.settlementserver.member.domain.Member;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class WorkerService {
+	public void registerWorker(Member member) {
+	}
+}
