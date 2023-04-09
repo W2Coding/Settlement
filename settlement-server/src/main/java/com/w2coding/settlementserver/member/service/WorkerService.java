85e13aafe35a0cf47b8f0a3a61a1700b5e99ad22
HoYoon-Lee <happylhy0630@naver.com>
2023-04-09 21:51:47 +0900

    FEATURE : delete registerWorker() from WorkerService

--- settlement-server/src/main/java/com/w2coding/settlementserver/member/service/WorkerService.java
+++ settlement-server/src/main/java/com/w2coding/settlementserver/member/service/WorkerService.java
@@ -1,14 +1,9 @@
 package com.w2coding.settlementserver.member.service;
 
-import org.springframework.stereotype.Service;
-
-import com.w2coding.settlementserver.member.domain.Member;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class WorkerService {
-	public void registerWorker(Member member) {
-	}
 }
