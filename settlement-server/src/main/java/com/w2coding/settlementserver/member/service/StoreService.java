a21b678f72924562b42d4be950e423c524722939
HoYoon-Lee <happylhy0630@naver.com>
2023-04-09 21:54:53 +0900

    FEATURE : add StoreService

---
+++ settlement-server/src/main/java/com/w2coding/settlementserver/member/service/StoreService.java
@@ -0,0 +1,15 @@
+package com.w2coding.settlementserver.member.service;
+
+import com.w2coding.settlementserver.member.domain.Store;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class StoreService {
+
+    public Store findById(Long id) {
+        return null;
+    }
+
+}
