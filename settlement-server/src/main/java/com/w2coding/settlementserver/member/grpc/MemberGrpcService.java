ad8c93030f5921de08e6f7109bf5b2fa19e108e5
HoYoon-Lee <happylhy0630@naver.com>
2023-04-16 22:32:07 +0900

    REFACTOR : delete exception handling logic from MemberGrpcService

--- settlement-server/src/main/java/com/w2coding/settlementserver/member/grpc/MemberGrpcService.java
+++ settlement-server/src/main/java/com/w2coding/settlementserver/member/grpc/MemberGrpcService.java
@@ -1,45 +1,27 @@
 package com.w2coding.settlementserver.member.grpc;
 
-import org.springframework.stereotype.Service;
-
 import com.w2coding.proto.common.GrpcResponse;
 import com.w2coding.proto.member.MemberServiceGrpc;
 import com.w2coding.proto.member.SignUpRequest;
-import com.w2coding.settlementserver.common.exception.GlobalException;
-import com.w2coding.settlementserver.common.exception.code.GlobalExceptionCode;
 import com.w2coding.settlementserver.member.dto.SignUpDto;
 import com.w2coding.settlementserver.member.service.MemberService;
-
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class MemberGrpcService extends MemberServiceGrpc.MemberServiceImplBase {
 
-	private final MemberService memberService;
+    private final MemberService memberService;
 
-	@Override
-	public void signUp(SignUpRequest request, StreamObserver<GrpcResponse> observer) {
-		try {
-			memberService.signUp(SignUpDto.of(request));
-			observer.onNext(GrpcResponse.newBuilder().build());
-			observer.onCompleted();
-		}
-		catch (GlobalException exception) {
-			GlobalExceptionCode exceptionCode = exception.getExceptionCode();
-			observer.onNext(GrpcResponse.newBuilder()
-					.setResult(exceptionCode.getCode())
-					.setMessage(exceptionCode.getMessage())
-				.build());
-			observer.onCompleted();
-		}
-		catch (Exception exception) {
-			log.error("Occurred unexpected error : {}", exception.getMessage());
-			observer.onError(exception);
-		}
-	}
+    @Override
+    public void signUp(SignUpRequest request, StreamObserver<GrpcResponse> observer) {
+        memberService.signUp(SignUpDto.of(request));
+        observer.onNext(GrpcResponse.newBuilder().build());
+        observer.onCompleted();
+    }
 
 }
