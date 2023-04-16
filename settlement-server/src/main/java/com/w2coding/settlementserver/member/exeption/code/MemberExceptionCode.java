cb051e48621f22bd28eb933b6ab1821beddd2a02
HoYoon-Lee <happylhy0630@naver.com>
2023-04-16 22:24:27 +0900

    REFACTOR : add status field to GlobalException

--- settlement-server/src/main/java/com/w2coding/settlementserver/member/exeption/code/MemberExceptionCode.java
+++ settlement-server/src/main/java/com/w2coding/settlementserver/member/exeption/code/MemberExceptionCode.java
@@ -2,6 +2,7 @@ package com.w2coding.settlementserver.member.exeption.code;
 
 import com.w2coding.settlementserver.common.exception.code.GlobalExceptionCode;
 
+import io.grpc.Status;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,10 +10,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MemberExceptionCode implements GlobalExceptionCode {
 
-	DUPLICATED_EMAIL("S_M001", "Duplicated email"),
-	DISABLED_STORE("S_M002", "Disabled store"),
+	DUPLICATED_EMAIL("S_M001", "Duplicated email", Status.ALREADY_EXISTS),
+	DISABLED_STORE("S_M002", "Disabled store", Status.FAILED_PRECONDITION),
 	;
 
 	private final String code;
 	private final String message;
+	private final Status status;
+
 }
