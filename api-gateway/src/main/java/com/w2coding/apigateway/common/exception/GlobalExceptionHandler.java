90e5193f785c5b7a90952c694d24d57a590a47e6
HoYoon-Lee <happylhy0630@naver.com>
2023-04-30 22:51:21 +0900

    FEATURE : modify error handling logic

--- api-gateway/src/main/java/com/w2coding/apigateway/common/exception/GlobalExceptionHandler.java
+++ api-gateway/src/main/java/com/w2coding/apigateway/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.w2coding.apigateway.common.exception;
 
 import com.w2coding.apigateway.common.response.Response;
+import io.grpc.Metadata;
+import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,15 +16,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(StatusRuntimeException.class)
     public ResponseEntity<?> handleStatusRuntimeException(StatusRuntimeException e) {
-        try {
-            GrpcException exception = (GrpcException) e.getCause();
-            log.error("Occurred gRPC Exception: {} {}", exception.getCode(), exception.getMessage());
-            return Response.error(exception.getHttpStatus(e.getStatus()), exception.getCode(), exception.getMessage());
-        }
-        catch (Exception exception) {
-            log.error("Occurred gRPC unknown Exception: {}", e.getMessage());
-            return Response.internalServerError();
-        }
+        String code = e.getTrailers().get(Metadata.Key.of("code", Metadata.ASCII_STRING_MARSHALLER));
+        String message = e.getTrailers().get(Metadata.Key.of("message", Metadata.ASCII_STRING_MARSHALLER));
+
+        log.error("Occurred gRPC Exception: {} {}", code, message);
+        return Response.error(getHttpStatus(e.getStatus()), code, message);
     }
 
     private HttpStatus getHttpStatus(Status status) {
