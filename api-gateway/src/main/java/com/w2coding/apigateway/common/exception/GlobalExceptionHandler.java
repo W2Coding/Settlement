09a561ba3bb0c2bd156bce08ad479d85104c95a5
HoYoon-Lee <happylhy0630@naver.com>
2023-04-30 22:49:56 +0900

    FEATURE : move getHttpStatus to GlobalExceptionHandler

--- api-gateway/src/main/java/com/w2coding/apigateway/common/exception/GlobalExceptionHandler.java
+++ api-gateway/src/main/java/com/w2coding/apigateway/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.w2coding.apigateway.common.exception;
 import com.w2coding.apigateway.common.response.Response;
 import io.grpc.StatusRuntimeException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,4 +25,21 @@ public class GlobalExceptionHandler {
         }
     }
 
+    private HttpStatus getHttpStatus(Status status) {
+        return switch (status.getCode()) {
+            case OK -> HttpStatus.OK;
+            case CANCELLED, DEADLINE_EXCEEDED -> HttpStatus.REQUEST_TIMEOUT;
+            case INVALID_ARGUMENT -> HttpStatus.BAD_REQUEST;
+            case NOT_FOUND -> HttpStatus.NOT_FOUND;
+            case ALREADY_EXISTS, ABORTED -> HttpStatus.CONFLICT;
+            case PERMISSION_DENIED -> HttpStatus.FORBIDDEN;
+            case RESOURCE_EXHAUSTED -> HttpStatus.TOO_MANY_REQUESTS;
+            case FAILED_PRECONDITION -> HttpStatus.PRECONDITION_FAILED;
+            case OUT_OF_RANGE -> HttpStatus.REQUESTED_RANGE_NOT_SATISFIABLE;
+            case UNIMPLEMENTED -> HttpStatus.NOT_IMPLEMENTED;
+            case UNAVAILABLE -> HttpStatus.SERVICE_UNAVAILABLE;
+            case UNKNOWN, INTERNAL, DATA_LOSS -> HttpStatus.INTERNAL_SERVER_ERROR;
+            default -> HttpStatus.INTERNAL_SERVER_ERROR;
+        };
+    }
 }
